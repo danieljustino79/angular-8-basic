a9838b7f3e663ec6363c84e4b4c8062370541f50
danieljustino79 <danieljustino79@gmail.com>
2019-07-25 23:31:38 -0300

    modulo1 com Module e Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { Modulo1Module } from './modulo1/modulo1.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, Modulo1Module ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
